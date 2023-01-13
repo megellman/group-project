070954ecd3d428bf1d716c8274bebf1cd523d089
Megan Ellman <meganlellman@gmail.com>
2023-01-13 15:17:37 -0800

    create save btn event listener to create save form and form submit function to save item to local storage

--- assets/results.js
+++ assets/results.js
@@ -90,4 +90,44 @@ function getDrinkBySpirit() {
   })
 }
 
+// Click save button to get form to select what party (key) to save recipe to
+resultsContainer.on('click', '.saveBtn', function(){
+  var currentContainer = this.parent();
+  var form = $('<form>');
+  var input = $('<input>');
+  var datalist = $('<datalist>');
 
+  input.attr({
+    type: 'text',
+    list: 'options'
+  });
+  datalist.attr('id', 'formOps');
+  form.attr('id', 'form');
+
+  form.append(input);
+  form.append(input);
+  currentContainer.append(form);
+
+  // If there are already form options saved in local storage, pull them down and make them options
+  if(localStorage.getItem('formObj') !== null){
+    var formOptions = JSON.parse(localStorage.getItem('formObj'));
+    for(var i = 0; i < formObj.length; i++){
+      var formEntry = $('<option>');
+      formEntry.text(formOptions[i])
+      form.append(formEntry);
+    } 
+  }
+})
+
+// When party (key) is selected, see if key already exists and if it does, get key data, add new objarr (recipe) to obj save to local storage. If it doesn't already exist, create a new key and save it to local storage
+$('form').on('submit', function(){
+  var optionSelected = option.value;
+  $('#form').remove();
+  var recipeText = this.parent().text();
+  if(localStorage.getItem(optionSelected) === null){
+    localStorage.setItem(optionSelected, recipeText);
+  } else {
+    var existingOption = JSON.parse(localStorage.getItem(optionSelected));
+    existingOption.push(recipeText);
+  }
+})
