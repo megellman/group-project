6f2324b4e810c8356486c73f62066a30f0a35d53
Megan Ellman <meganlellman@gmail.com>
2023-01-13 14:00:12 -0800

    update functions based on feedback

--- assets/results.js
+++ assets/results.js
@@ -9,122 +9,85 @@ var backBtn = $('#backBtn')
 
 
 // this says depending on what parameters the user choose run the corresponding function 
-if (wine != "") {
-  console.log(wine !== "")
-  getWineParing()
-  wine = "";
+if (wine != ""){
+    console.log(wine !== "")
+    getWineParing()
+    wine = ""
 } else if (spirit !== "") {
-  getDrinkBySpirit()
-  spirit = ""
-// } else if (food !== "") {
-//   getRecipes()
-//   food = ""
-// } 
+    getDrinkBySpirit()
+    spirit = ""
+} else if (food !== "") {
+    getRecipes()
+    food = ""
 } else if (drinkName !== "") {
-  getDrinkByName()
-  drinkName = ""
-} else {
-  (random)
-  randomDrink()
-  // Homepage - the initial value is going to be false and then there is going to be an event listener that sets this to true
-  random = false
-}
+    getDrinkByName()
+    drinkName = ""
+} else { (random) 
+    randomDrink()
+    // Homepage - the initial value is going to be false and then there is going to be an event listener that sets this to true
+    random = false
+} 
 
 
 // Get Wine Pairings
-function getWineParing() {
-  fetch(pairingUrl)
-    .then(function (response) {
-      return response.json();
+function getWineParing(){
+fetch(pairingUrl)
+    .then(function(response){
+        return response.json();
     })
-    .then(function (data) {
-      console.log(data);
-      var resultEntry = $('<div>');
-      var saveBtn = $('<button>');
-      saveBtn.text('X');
-      resultEntry.text(data.text);
-      resultEntry.append(saveBtn);
-      resultsContainer.append(resultEntry);
+    .then(function (data){
+        console.log(data);
+        var resultEntry = $('<div>');
+        var saveBtn = $('<button');
+        saveBtn.text =
+        resultEntry.text(data.text);
+        resultsContainer.append(resultEntry);
+
     })
 }
 
 // Get Recipes
-// function getRecipes() {
-//   fetch(recipeUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     })
-// }
+function getRecipes(){
+fetch(recipeUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+}
 
 // fetching a recipe by drink name 
 function getDrinkByName() {
-  fetch(cocktailURLDrinkName)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (dataDrinkName) {
-      console.log(dataDrinkName);
-      for (var i = 0; i < 5; i++) {
-        var resultEntry = $('<div>');
-        var saveBtn = $('<button>');
-        var cocktailImg = $('<img>');
-        cocktailImg.attr('src', dataDrinkName.drinks.strDrinkThumb);
-        saveBtn.text('X');
-        resultEntry.text(dataDrinkName.drinks[i].strInstructions);
-        resultEntry.append(cocktailImg);
-        resultEntry.append(saveBtn);
-        resultsContainer.append(resultEntry);
-      }
-    })
+fetch(cocktailURLDrinkName) 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(dataDrinkName){
+    console.log(dataDrinkName)
+  })
 }
 
 // fetching a list by spirit 
 function getDrinkBySpirit() {
-  fetch(cocktailURLSpirit)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (dataSpirit) {
-      console.log(dataSpirit);
-      for (var i = 0; i < 5; i++) {
-        drinkName = dataSpirit.drinks[i].strDrink;
-        getRecipeFromSpirit()
-      }
-    })
+  fetch(cocktailURLSpirit) 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(dataSpirit){
+  console.log(dataSpirit)
+  })
 }
 
-// fetching a recipe by drink name 
-function getRecipeFromSpirit() {
-  fetch(drinkNameUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (dataDrinkName) {
-      console.log(dataDrinkName);
-      var resultEntry = $('<div>');
-      var saveBtn = $('<button>');
-      var cocktailImg = $('<img>');
-      cocktailImg.attr('src', dataDrinkName.drinks[0].strDrinkThumb);
-      saveBtn.text('X');
-      resultEntry.text(dataDrinkName.drinks.strInstructions);
-      resultEntry.append(cocktailImg);
-      resultEntry.append(saveBtn);
-      resultsContainer.append(resultEntry);
-    })
-}
-
-// fetching a Random drink
-function randomDrink() {
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (dataRandom) {
-      console.log(dataRandom)
-    })
+  // fetching a Random drink
+  function randomDrink() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php') 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(dataRandom){
+    console.log(dataRandom)
+  })
 }
 
 
