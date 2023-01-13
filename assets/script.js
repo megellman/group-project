cda0acc118ebb2a545c1246f411f530ec55ba061
Megan Ellman <meganlellman@gmail.com>
2023-01-13 13:20:06 -0800

    added functions to put search results on page

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,20 @@
 //Parameter Variables
-var food = "pasta";
-var drinkName = "margarita"
-var spirit = "vodka"
-var wine = "malbec";
+var food = "";
+var drinkName = "";
+var spirit = "gin";
+var wine = "";
+var drinkFromSpirit = "";
 
 //API URL's
 var recipeApiKey =  "20f9574ee747498490dd1bd80b379967";
 //this wine pairing is give it a wine and get a food
 var pairingUrl = `https://api.spoonacular.com/food/wine/dishes?wine=${wine}&apiKey=${recipeApiKey}`;
 var recipeUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${food}&number=2&apiKey=20f9574ee747498490dd1bd80b379967`;
-var cocktailURLSpirit = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&i=${spirit}`
-var cocktailURLDrinkName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&s=${drinkName}`
+var cocktailURLSpirit = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spirit}`;
+
+var cocktailURLDrinkName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&s=${drinkName}`;
+
+var drinkNameUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&s=${drinkFromSpirit}`;
 
 // Get Wine Pairings
 // fetch(pairingUrl)
