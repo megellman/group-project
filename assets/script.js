5b783d9f9b061ee5b57565396a07b3dc1157e671
Becka McNally <beckamcnally@gmail.com>
2023-01-12 21:56:07 -0800

    initail TheCocktailDB fetch request

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,43 @@
-// const options = {
-// 	method: 'GET',
-//     cache: 'default',
-//     headers: {
-//         'content-type': 'application/json'}
-// };
 
-// const url = `https://v6.exchangerate-api.com/v6/01530c6661485ebabecd4c72/latest/USD`
-// var response = options + fetch(url)
-// var data = response.value
+var drinkName = "margarita"
 
+var spirit = "vodka"
+var cocktailURLSpirit = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&i=` + spirit
+var cocktailURLDrinkName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&s=${drinkName}`
+// fetching a recipe by drink name 
+fetch(cocktailURLDrinkName) 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(dataDrinkName){
+    console.log(dataDrinkName)
+  })
 
-// console.log(url, options)
+  // fetching a list by spirit 
+fetch(cocktailURLDrinkName) 
+.then(function (response) {
+  return response.json();
+})
+.then(function(dataSpirit){
+  console.log(dataSpirit)
+})
 
+  // fetching a Random drink
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php") 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(dataRandom){
+    console.log(dataRandom)
+  })
 
-// Example POST method implementation:
-async function postData(url = 'https://v6.exchangerate-api.com/v6/01530c6661485ebabecd4c72/latest/USD', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'include', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
+  // //
+  // fetch(cocktailURLDrinkName) 
+  // .then(function (response) {
+  //   return response.json();
+  // })
+  // .then(function(dataDrinkName){
+  //   console.log(dataDrinkName)
+  // })
   
-  postData('https://v6.exchangerate-api.com/v6/01530c6661485ebabecd4c72/latest/USD', { answer: 42 })
-    .then((data) => {
-      console.log(data); // JSON data parsed by `data.json()` call
-    });
+ 
