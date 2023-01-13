a4d1c536ffb87cd160eea22ab25b263a97696e9c
Megan Ellman <meganlellman@gmail.com>
2023-01-12 21:43:41 -0800

    put variables at the top

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Variables
+var recipeApiKey =  "20f9574ee747498490dd1bd80b379967";
 var wine = "malbec";
 var pairingUrl = `https://api.spoonacular.com/food/wine/dishes?wine=${wine}&apiKey=${recipeApiKey}`;
-var recipeApiKey =  "20f9574ee747498490dd1bd80b379967";
 var food = "pasta";
 var recipeUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${food}&number=2&apiKey=20f9574ee747498490dd1bd80b379967`;
 
