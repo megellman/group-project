ba7f66a90b2c98c98792b65bc697aadf035fd9c9
Becka McNally <beckamcnally@gmail.com>
2023-01-15 11:45:48 -0800

    food form info saving to local storage and pulling from local storage hitting apiCalls running smooth

--- assets/script.js
+++ assets/script.js
@@ -18,28 +18,25 @@ var hungryForm = $('#hungryForm')
 var food = "";
 
 var drinkName = ""
-// var spirit does not have input, change checkbox? Maybe dropdown menu w/ spirit type
+// var spirit does not have input, change checkbox? Maybe dropdown menu w/ spirit type 
 var wine = "";
+localStorage.setItem('wine', wine)
+var pairingUrl;
 // NO input for wine as of rn
 var random = false
+localStorage.setItem('random', random)
 // if the user is looking for a pairing of a wine for a particular food then they will have to give us the food and check the box for pairing 
 var wineP = false
-
+localStorage.setItem('wineP', wineP)
 //API URL's
-var recipeApiKey =  "0ed1c23457ba46ddaffacdeb0b81d967"; //"20f9574ee747498490dd1bd80b379967"; 
+
 //this wine pairing is give it a wine and get a food
-var pairingUrl = `https://api.spoonacular.com/food/wine/dishes?wine=${wine}&apiKey=${recipeApiKey}`;
-// this wine pairing is give it a food and get a wine
-var foodPairingUrl = `https://api.spoonacular.com/food/wine/pairing?food=${food}&apiKey=${recipeApiKey}`;
-// if we split our recipe searches and wine pairing searches between different api's we won't max out so easily
-var edrecipeUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_key=d0e7ce8996da109b870161b5504f5e87&app_id=b7a56f5e`;
+    // this wine pairing is give it a food and get a wine
+var foodPairingUrl;
+var edrecipeUrl;
+var cocktailURLDrinkName;
 
-// event listener for homepage foodBtn - hides both buttons and unhides food form
-
-
-// var recipeUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${food}&number=2&apiKey=20f9574ee747498490dd1bd80b379967&addRecipeInformation=true`;
-
-var cocktailURLDrinkName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&s=${drinkName}`;
+var userParameters= [];
 
 
 foodButton.on("click", function(e){
@@ -62,11 +59,10 @@ cocktailButton.on('click', function(e){
 foodSubmit.on('click', function(e){ 
   // hungryForm.on('click', '#foodSubmit', function(e){ 
   e.preventDefault()
-  e.isPre
   food = foodInput.val()
-  console.log(food)
-  // location.assign('results.html')
-
+  localStorage.setItem('food', food)
+  newFunction()
+  return food;// might be dead
 })
 
 
@@ -75,7 +71,8 @@ foodSubmit.on('click', function(e){
 cocktailSubmit.on('submit', function(e){ 
   e.preventDefault()
   drinkName = cocktailInput.val()
-  location.assign('results.html')
+  localStorage.setItem('drinkName', drinkName)
+  
 })
 
  // toggles wine p value true/false to if button is checked
@@ -97,3 +94,9 @@ cocktailSubmit.on('submit', function(e){
       random = false
     } 
   })
+
+  
+
+  function newFunction() {
+    location.assign('./results.html')
+  }
