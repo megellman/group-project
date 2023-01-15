b70c60bc0f775e6742ea81a3cfbde09440964afd
Megan Ellman <meganlellman@gmail.com>
2023-01-15 13:16:53 -0800

    added if else statements for adding to local storage

--- assets/results.js
+++ assets/results.js
@@ -24,32 +24,35 @@ function pairedWineRecipes() {
     })
     .then(function (dataPF) {
       console.log(dataPF)
-      
-        var dh = dataPF.hits[0]
-        var resultCardContainer = $('<div>')
-        var saveBtn = $('<button>');
-        var recipeImg = $('<img>');
-        var nameOfRecipe = $('<h2>');
-        var linkToRecipe = $('<a>')
 
-        recipeImg.attr('src', dataPF.hits[0].recipe.image);
-        saveBtn.text('save');
-        saveBtn.attr('class', 'saveBtn');
-        recipeImg.attr('class', 'object-scale-down h-48 w-96')
-        //   resultEntry.attr('class', 'border-4 border-green-800 border-double my-5')
-        linkToRecipe.attr('href', dh.recipe.url)
-        resultCardContainer.attr('class', 'recipe px-6 py-4 border-4 border-green-800 border-double my-5')
-        nameOfRecipe.attr('class', 'font-bold text-xl mb-2')
+      var dh = dataPF.hits[0]
+      var resultCardContainer = $('<div>')
+      var saveBtn = $('<button>');
+      var recipeImg = $('<img>');
+      var nameOfRecipe = $('<h2>');
+      var linkToRecipe = $('<a>')
 
-        nameOfRecipe.text(dh.recipe.label)
-        linkToRecipe.text('Go to Recipe')
+      recipeImg.attr('src', dataPF.hits[0].recipe.image);
+      saveBtn.text('save');
+      saveBtn.attr('class', 'saveBtn');
+      recipeImg.attr('class', 'object-scale-down h-48 w-96')
+      //   resultEntry.attr('class', 'border-4 border-green-800 border-double my-5')
+      linkToRecipe.attr([{
+        'href': dh.recipe.url,
+        'class': 'recipe-content'
+      }])
+      resultCardContainer.attr('class', 'recipe px-6 py-4 border-4 border-green-800 border-double my-5')
+      nameOfRecipe.attr('class', 'font-bold text-xl mb-2')
 
-        resultsContainer.append(resultCardContainer)
-        resultCardContainer.append(recipeImg)
-        resultCardContainer.append(nameOfRecipe)
-        resultCardContainer.append(linkToRecipe)
-        resultCardContainer.append(saveBtn)
-      }
+      nameOfRecipe.text(dh.recipe.label)
+      linkToRecipe.text('Go to Recipe')
+
+      resultsContainer.append(resultCardContainer)
+      resultCardContainer.append(recipeImg)
+      resultCardContainer.append(nameOfRecipe)
+      resultCardContainer.append(linkToRecipe)
+      resultCardContainer.append(saveBtn)
+    }
 
     )
 }
@@ -69,9 +72,14 @@ function getFoodPairing() {
       var wineLink = $('<a>');
       var saveBtn = $('<button>');
 
-      wineImg.attr('src', data.productMatches[0].imageUrl)
-      wineLink.attr('href', data.productMatches[0].link)
+      wineImg.attr('src', data.productMatches[0].imageUrl);
+      wineLink.attr({
+        'href': data.productMatches[0].link,
+        'class': 'recipe-content'
+      });
       resultEntry.attr('class', 'recipe px-6 py-4 border-4 border-green-800 border-double my-5')
+      pairMatch.attr('class', 'recipe-content');
+      pairingText.attr('class', 'recipe-content');
 
       pairMatch.text('Top Choice: ' + data.productMatches[0].title)
       saveBtn.text('Save');
@@ -96,7 +104,6 @@ function getRecipes() {
     })
     .then(function (data) {
       console.log(data);
-
       for (var i = 0; i < 5; i++) {
         var dh = data.hits[i]
         var resultEntry = $('<div>');
@@ -106,12 +113,17 @@ function getRecipes() {
         var nameOfRecipe = $('<h2>');
         var linkToRecipe = $('<a>')
 
-        recipeImg.attr('src', data.hits[i].recipe.image);
-        recipeImg.attr('class', 'object-scale-down h-48 w-96')
+        recipeImg.attr({
+          'src': data.hits[i].recipe.image,
+          'class': 'object-scale-down h-48 w-96'
+        });
         saveBtn.text('save');
         saveBtn.attr('class', 'saveBtn');
         //   resultEntry.attr('class', 'border-4 border-green-800 border-double my-5')
-        linkToRecipe.attr('href', dh.recipe.url)
+        linkToRecipe.attr({
+          'href': dh.recipe.url,
+          'class': 'recipe-content'
+        });
         resultCardContainer.attr('class', 'recipe px-6 py-4 border-4 border-green-800 border-double my-5')
         nameOfRecipe.attr('class', 'font-bold text-xl mb-2')
 
@@ -139,39 +151,45 @@ function getDrinkByName() {
     })
     .then(function (dataDrinkName) {
       console.log(dataDrinkName);
-      var entryContainer = $('<div>').appendTo(resultsContainer);
-      var drinkName = $('<h2>').appendTo(entryContainer);
-      var ingredients = $('<p>').appendTo(entryContainer)
-      var instructions = $('<p>').appendTo(entryContainer);
-      var measurements = $('<p>').appendTo(entryContainer);
-      var thumbNail = $('<img>').appendTo(entryContainer);
-      var saveBtn = $('<button>').appendTo(entryContainer);
-      // URL issue, if time, revisit this
-      // var video = $('<iframe>').appendTo(entryContainer);
+      for (var i = 0; i < 5; i++) {
+        var entryContainer = $('<div>').appendTo(resultsContainer);
+        var drinkName = $('<h2>').appendTo(entryContainer);
+        var ingredients = $('<p>').appendTo(entryContainer)
+        var instructions = $('<p>').appendTo(entryContainer);
+        var measurements = $('<p>').appendTo(entryContainer);
+        var thumbNail = $('<img>').appendTo(entryContainer);
+        var saveBtn = $('<button>').appendTo(entryContainer);
+        // URL issue, if time, revisit this
+        // var video = $('<iframe>').appendTo(entryContainer);
 
-      saveBtn.text('save');
-      saveBtn.attr('class', 'saveBtn');
-      thumbNail.attr('src', dataDrinkName.drinks[0].strDrinkThumb);
-      entryContainer.attr('class', 'recipe');
-      // video.attr({
-      //   src: dataDrinkName.drinks[0].strVideo,
-      //   class: 'aspect-video'
-      // });
+        instructions.attr('class', 'recipe-content');
+        measurements.attr('class', 'recipe-content');
+        ingredients.attr('class', 'recipe-content');
+        saveBtn.text('save');
+        saveBtn.attr('class', 'saveBtn');
+        thumbNail.attr('src', dataDrinkName.drinks[i].strDrinkThumb);
+        entryContainer.attr('class', 'recipe');
+        // video.attr({
+        //   src: dataDrinkName.drinks[0].strVideo,
+        //   class: 'aspect-video'
+        // });
 
-      var arr = [dataDrinkName.drinks[0].strIngredient1, dataDrinkName.drinks[0].strIngredient2, dataDrinkName.drinks[0].strIngredient3, dataDrinkName.drinks[0].strIngredient4, dataDrinkName.drinks[0].strIngredient5, dataDrinkName.drinks[0].strIngredient6, dataDrinkName.drinks[0].strIngredient7, dataDrinkName.drinks[0].strIngredient8, dataDrinkName.drinks[0].strIngredient9, dataDrinkName.drinks[0].strIngredient10, dataDrinkName.drinks[0].strIngredient11, dataDrinkName.drinks[0].strIngredient12, dataDrinkName.drinks[0].strIngredient13, dataDrinkName.drinks[0].strIngredient14, dataDrinkName.drinks[0].strIngredient15];
-      var measArr = [dataDrinkName.drinks[0].strMeasure1, dataDrinkName.drinks[0].strMeasure2, dataDrinkName.drinks[0].strMeasure3, dataDrinkName.drinks[0].strMeasure, dataDrinkName.drinks[0].strMeasure4, dataDrinkName.drinks[0].strMeasure5, dataDrinkName.drinks[0].strMeasure6, dataDrinkName.drinks[0].strMeasure7, dataDrinkName.drinks[0].strMeasure8, dataDrinkName.drinks[0].strMeasure9, dataDrinkName.drinks[0].strMeasure10, dataDrinkName.drinks[0].strMeasure11, dataDrinkName.drinks[0].strMeasure12, dataDrinkName.drinks[0].strMeasure13, dataDrinkName.drinks[0].strMeasure14, dataDrinkName.drinks[0].strMeasure15]
+        var arr = [dataDrinkName.drinks[i].strIngredient1, dataDrinkName.drinks[i].strIngredient2, dataDrinkName.drinks[i].strIngredient3, dataDrinkName.drinks[i].strIngredient4, dataDrinkName.drinks[i].strIngredient5, dataDrinkName.drinks[i].strIngredient6, dataDrinkName.drinks[i].strIngredient7, dataDrinkName.drinks[i].strIngredient8, dataDrinkName.drinks[i].strIngredient9, dataDrinkName.drinks[i].strIngredient10, dataDrinkName.drinks[i].strIngredient11, dataDrinkName.drinks[i].strIngredient12, dataDrinkName.drinks[i].strIngredient13, dataDrinkName.drinks[i].strIngredient14, dataDrinkName.drinks[i].strIngredient15];
+        var measArr = [dataDrinkName.drinks[i].strMeasure1, dataDrinkName.drinks[i].strMeasure2, dataDrinkName.drinks[i].strMeasure3, dataDrinkName.drinks[i].strMeasure, dataDrinkName.drinks[i].strMeasure4, dataDrinkName.drinks[i].strMeasure5, dataDrinkName.drinks[i].strMeasure6, dataDrinkName.drinks[i].strMeasure7, dataDrinkName.drinks[i].strMeasure8, dataDrinkName.drinks[i].strMeasure9, dataDrinkName.drinks[i].strMeasure10, dataDrinkName.drinks[i].strMeasure11, dataDrinkName.drinks[i].strMeasure12, dataDrinkName.drinks[i].strMeasure13, dataDrinkName.drinks[i].strMeasure14, dataDrinkName.drinks[i].strMeasure15]
 
-      var ingredientItems = arr.filter(function(el){
-        return el != null;
-      });
-      var measurementItems = measArr.filter(function(el){
-        return el != null;
-      });
-      
-      instructions.text(dataDrinkName.drinks[0].strInstructions);
-      drinkName.text(dataDrinkName.drinks[0].strDrink);
-      measurements.text(measurementItems);
-      ingredients.text(ingredientItems);
+        var ingredientItems = arr.filter(function (el) {
+          return el != null;
+        });
+        var measurementItems = measArr.filter(function (el) {
+          return el != null;
+        });
+
+        instructions.text(dataDrinkName.drinks[i].strInstructions);
+        drinkName.text(dataDrinkName.drinks[i].strDrink);
+        measurements.text(measurementItems);
+        ingredients.text(ingredientItems);
+
+      }
     })
 }
 
@@ -193,6 +211,8 @@ function randomDrink() {
       var drinkRecipe = $('<a>');
       var instructions = $('<p>');
 
+      drinkRecipe.attr('class', 'recipe-content');
+      instructions.attr('class', 'recipe-content');
       resultEntry.attr('class', 'recipe border-4 border-green-800 border-double my-5')
       cocktailImg.attr('src', dataR.strDrinkThumb);
       cocktailImg.attr('class', 'object-scale-down h-48 w-96');
@@ -231,7 +251,6 @@ function randomDrink() {
 
 // this says depending on what parameters the user choose run the corresponding function 
 function apiCalls() {
-  console.log ("madits " + edrecipeUrl + 'and ' + food)
   if (wine !== "") {
     getWinePairing()
     wine = "";
@@ -240,14 +259,13 @@ function apiCalls() {
     food = "";
     wineP = false
   } else if (food !== "") {
-    console.log('made it to here going to getRecipes')
     getRecipes()
     food = "";
-    localStorage.setItem('food', food)
+
   } else if (drinkName !== "") {
     getDrinkByName()
     drinkName = ""
-  } else if(random == true){
+  } else if (random == true) {
     randomDrink()
     // Homepage - the initial value is going to be false and then there is going to be an event listener that sets this to true
     random = false
@@ -261,23 +279,23 @@ function setURL() {
   random = localStorage.getItem("random")
   wineP = localStorage.getItem("wineP")
 
-  var recipeApiKey =  "0ed1c23457ba46ddaffacdeb0b81d967"; //"20f9574ee747498490dd1bd80b379967"; 
+  var recipeApiKey = "0ed1c23457ba46ddaffacdeb0b81d967"; //"20f9574ee747498490dd1bd80b379967"; 
   pairingUrl = `https://api.spoonacular.com/food/wine/dishes?wine=${wine}&apiKey=${recipeApiKey}`;
   // this wine pairing is give it a food and get a wine
   foodPairingUrl = `https://api.spoonacular.com/food/wine/pairing?food=${food}&apiKey=${recipeApiKey}`;
   edrecipeUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_key=d0e7ce8996da109b870161b5504f5e87&app_id=b7a56f5e`;
-  
+
   cocktailURLDrinkName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&s=${drinkName}`;
 
-    apiCalls()
+  apiCalls()
 };
+
 // Click save button to get form to select what party (key) to save recipe to
 resultsContainer.on('click', '.saveBtn', function () {
-  console.log('hello');
   var currentContainer = $(this).parent();
   var form = $('<form>');
   var input = $('<input>');
-  var datalist = $('<datalist>');
+  var select = $('<select>');
   var submit = $('<input>');
 
   input.attr({
@@ -285,7 +303,7 @@ resultsContainer.on('click', '.saveBtn', function () {
     list: 'options',
     class: 'input'
   });
-  datalist.attr('id', 'options');
+  select.attr('id', 'options');
   form.attr('id', 'form');
   submit.attr({
     type: 'submit',
@@ -295,57 +313,66 @@ resultsContainer.on('click', '.saveBtn', function () {
 
   form.append(input);
   form.append(submit);
-  form.append(datalist);
+  form.append(select);
   currentContainer.append(form);
 
   // If there are already form options saved in local storage, pull them down and make them options
   // If this exists,
-  if (localStorage.getItem('formObj')) {
+  if (localStorage.getItem(('formObj').length == 1)) {
+    var formObj = localStorage.getItem("formObj");
+    var arr = [];
+    arr.push(formObj);
+    console.log(`type is ${typeof formObj}`);
+    for (var i = 0; i < formObj.length; i++) {
+      var formEntry = $('<option>');
+      console.log(formObj[i]);
+      formEntry.text(formObj[i]);
+      select.append(formEntry);
+    }
+  } else if (localStorage.getItem(JSON.parse('formObj').length > 1)) {
     console.log('load form options');
     var formObj = JSON.parse(localStorage.getItem('formObj'));
     console.log(formObj);
     console.log(`type is ${typeof formObj}`);
     for (var i = 0; i < formObj.length; i++) {
       var formEntry = $('<option>');
-      formEntry.text(formObj[i])
-      datalist.append(formEntry);
+      console.log(formObj[i]);
+      formEntry.text(formObj[i]);
+      select.append(formEntry);
     }
   } else if (localStorage.getItem('formObj') == null) {
     console.log('local storage empty, waiting');
     return
-}
-})
-
-$(document).on('click', '#submit', function (e) {
-  console.log('form submitted!');
-  e.preventDefault();
-  console.log('save to formObj');
-  if(localStorage.getItem('formObj') == null){
-    localStorage.setItem('formObj', JSON.stringify($('.input').val()));
-  } else {
-    var formOption = JSON.parse(localStorage.getItem('formObj'));
-    var formObj = [];
-    formObj.push(formOption);
-    formObj.push($('.input').val());
-    localStorage.setItem('formObj', JSON.stringify(formObj));
-  }
-  var recipeObj = {};
-  var newKey = $('.input').val();
-  var nestedKey = $(this).closest('h2').text();
-  var nestedVal = {content}
-  var nestedObj = {nestedKey, nestedVal}
-  console.log(`option selected is ${optionSelected}`);
-  var newObj = {newKey, nestedObj}
-  if (localStorage.getItem(optionSelected) === null) {
-    localStorage.setItem(recipeObj, newObj);
-    console.log('Saved!');
-  } else {
-    recipeObj = JSON.parse(localStorage.getItem(optionSelected));
-    console.log(existingOption);
-    console.log(optionSelected);
-    existingOption.push(recipeText);
   }
 })
 
-
-$(this).closest('.recipe'.children('option'.content()))
+  $(document).on('click', '#submit', function (e) {
+    console.log('form submitted!');
+    e.preventDefault();
+    console.log('save to formObj');
+    if (localStorage.getItem('formObj') == null) {
+      localStorage.setItem('formObj', JSON.stringify($('.input').val()));
+    } else {
+      var formOption = JSON.parse(localStorage.getItem('formObj'));
+      var formObj = [];
+      formObj.push(formOption);
+      formObj.push($('.input').val());
+      localStorage.setItem('formObj', JSON.stringify(formObj));
+    }
+    var recipeObj = {};
+    var newKey = $('.input').val();
+    var nestedKey = $(this).closest('h2').text();
+    var nestedVal = $(this).closest('.recipe').children('.recipe-content');
+    var nestedObj = { nestedKey, nestedVal }
+    console.log(`option selected is ${newKey}`);
+    var newObj = { newKey, nestedObj }
+    if (localStorage.getItem('recipeObj') === null) {
+      localStorage.setItem('recipeObj', newObj);
+      console.log('Saved!');
+    } else {
+      recipeObj = JSON.parse(localStorage.getItem('recipeObj'));
+      console.log(existingOption);
+      console.log(optionSelected);
+      existingOption.push(recipeText);
+    }
+  })
