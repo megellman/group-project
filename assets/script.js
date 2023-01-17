7e625107fa7a231051a2942ee663d39cbc4919d8
Becka McNally <beckamcnally@gmail.com>
2023-01-17 11:20:23 -0800

    nav works still has duplication bug

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,8 @@ var foodWine = $("#food-wine");
 var cocktails =$("#cocktails");
 var logoHome =$("#logo-home");
 var rfContainer = $('#search-form-container')
+var rContainer = $(' r-container')
+var btnClicked = true
 
 //Parameter Variables 
 
@@ -45,38 +47,7 @@ foodButton.on("click", function(e){
   foodForm.attr('class', 'hidden')
   cocktailForm.attr('class', 'hidden')
   form2.attr('class', 'hidden')
-
-  var divEl = $('<div>')
-  var formEl = $('<form>')
-  var h3El = $('<h3>')
-  var labelEl = $('<label>')
-  var inputEL = $('<input>')
-  var label2El = $('<label>')
-  var input2EL = $('<input>')
-  var input3El = $('<input>')
-  var buttonEL = $('<button>')
-
- divEl.attr( 'id', "form-1")
- formEl.attr('id', "hungryForm")
- h3El.text('Hungry')
- labelEl.attr('for', "food-name")
- labelEl.text('Food')
- inputEL.attr({'type':"text", 'id':"food-input"})
- label2El.attr('for', "wine-name")
- input2EL.attr({'type':"text", 'id':"wine-input"})
- input3El.attr({'type':"checkbox",  'id':"mycheckbox"})
- buttonEL.attr('id',"food-submit")
- buttonEL.text('Search')
-
- rfContainer.append(divEl)
-  divEl.append(formEl)
-  formEl.append(h3El)
-  formEl.append(labelEl)
-  labelEl.append(inputEL)
-  formEl.append(label2El)
-  label2El.append(input2EL)
-  formEl.append(input3El)
-  formEl.append(buttonEL)
+  foodFormGen()
 })
 
 //click cocktail btn on first view of home page
@@ -85,34 +56,7 @@ cocktailButton.on('click', function(e){
   foodForm.attr('class', 'hidden')
   cocktailForm.attr('class', 'hidden') 
   form1.attr('class', 'hidden')
-  
-   var divEl = $('<div>')
-   var formEl = $('<form>')
-   var h3El = $('<h3>')
-   var labelEl = $('<label>')
-   var inputEL = $('<input>')
-   var buttonEL = $('<button>')
-   var button2EL = $('<button>')
- 
-  divEl.attr( 'id',"form-2")
-  formEl.attr('id', "hungryForm")
-  h3El.text('Thirsty')
-  labelEl.attr('for', "drink-name")
-  labelEl.text('Enter Drink ')
-  inputEL.attr({'type':"text", 'id':"cocktail-input"})
-  
-  buttonEL.attr('id',"cocktails-submit")
-  buttonEL.text('Search')        
-  button2EL.attr({'id':"feeling-advent", 'class':'value=false'})
-  button2EL.text('Feeling Adventurous?')       
- 
-  rfContainer.append(divEl)
-   divEl.append(formEl)
-   formEl.append(h3El)
-   formEl.append(labelEl)
-   labelEl.append(inputEL)
-   formEl.append(buttonEL)
-   formEl.append(button2EL)
+  cocktailFormGen()
 })
 
 // click food wine tab at top
@@ -120,41 +64,11 @@ foodWine.on("click", function(e){
   e.stopPropagation()
   foodForm.attr('class', 'hidden')
   cocktailForm.attr('class', 'hidden')
+  var form2 = $('#form-2')
   form2.attr('class', 'hidden')
-  
-  // sfContainer.attr("class", "hidden")
-  // resultsContainer.attr("class", "hidden")
-  var divEl = $('<div>')
-  var formEl = $('<form>')
-  var h3El = $('<h3>')
-  var labelEl = $('<label>')
-  var inputEL = $('<input>')
-  var label2El = $('<label>')
-  var input2EL = $('<input>')
-  var input3El = $('<input>')
-  var buttonEL = $('<button>')
-
- divEl.attr('id',"form-1")
- formEl.attr('id', "hungryForm")
- h3El.text('Hungry')
- labelEl.attr('for', "food-name")
- labelEl.text('Food')
- inputEL.attr({'type':"text", 'id':"food-input"})
- label2El.attr('for', "wine-name")
- input2EL.attr({'type':"text", 'id':"wine-input"})
- input3El.attr({'type':"checkbox",  'id':"mycheckbox"})
- buttonEL.attr('id',"food-submit")
- buttonEL.text('Search')
-
- rfContainer.append(divEl)
-  divEl.append(formEl)
-  formEl.append(h3El)
-  formEl.append(labelEl)
-  labelEl.append(inputEL)
-  formEl.append(label2El)
-  label2El.append(input2EL)
-  formEl.append(input3El)
-  formEl.append(buttonEL)
+  var rc = $('#r-container')
+  rc.attr("class", "hidden")
+ foodFormGen()
 })
 
 
@@ -163,38 +77,12 @@ cocktails.on('click', function(e){
   console.log('i was clicked')
   foodForm.attr('class', 'hidden')
   cocktailForm.attr('class', 'hidden')
+  var form1 = $('#form-1')
   form1.attr('class', 'hidden')
-
-   // If this exists then we need to do all of these 
-  //  sfContainer.attr("class", "hidden")
-  //  resultsContainer.attr("class", "hidden")
-   var divEl = $('<div>')
-   var formEl = $('<form>')
-   var h3El = $('<h3>')
-   var labelEl = $('<label>')
-   var inputEL = $('<input>')
-   var buttonEL = $('<button>')
-   var button2EL = $('<button>')
- 
-  divEl.attr('id', "form-2")
-  formEl.attr('id', "hungryForm")
-  h3El.text('Thirsty')
-  labelEl.attr('for', "drink-name")
-  labelEl.text('Enter Drink ')
-  inputEL.attr({'type':"text", 'id':"cocktail-input"})
-  
-  buttonEL.attr('id',"cocktails-submit")
-  buttonEL.text('Search')//          
-  button2EL.attr({'id':"feeling-advent", 'class':value=false})
-  button2EL.text('Feeling Adventurous?')       
- 
-  rfContainer.append(divEl)
-   divEl.append(formEl)
-   formEl.append(h3El)
-   formEl.append(labelEl)
-   labelEl.append(inputEL)
-   formEl.append(buttonEL)
-   formEl.append(button2EL)
+  var rc = $('#r-container')
+  rc.attr("class", "hidden")
+    
+  cocktailFormGen()
 })
 
 // event listener submit button on food form will add it form as onSubmit after get the variable to work on results.js
@@ -248,3 +136,68 @@ $(document).on('click', '#cocktails-submit', function(e){
       location.assign('./results.html')
     }
   })
+
+
+  function foodFormGen() {
+  var divEl = $('<div>')
+  var formEl = $('<form>')
+  var h3El = $('<h3>')
+  var labelEl = $('<label>')
+  var inputEL = $('<input>')
+  var label2El = $('<label>')
+  var input2EL = $('<input>')
+  var input3El = $('<input>')
+  var buttonEL = $('<button>')
+
+ divEl.attr( 'id', "form-1")
+ formEl.attr('id', "hungryForm")
+ h3El.text('Hungry')
+ labelEl.attr('for', "food-name")
+ labelEl.text('Food')
+ inputEL.attr({'type':"text", 'id':"food-input"})
+ label2El.attr('for', "wine-name")
+ input2EL.attr({'type':"text", 'id':"wine-input"})
+ input3El.attr({'type':"checkbox",  'id':"mycheckbox"})
+ buttonEL.attr('id',"food-submit")
+ buttonEL.text('Search')
+
+ rfContainer.append(divEl)
+  divEl.append(formEl)
+  formEl.append(h3El)
+  formEl.append(labelEl)
+  labelEl.append(inputEL)
+  formEl.append(label2El)
+  label2El.append(input2EL)
+  formEl.append(input3El)
+  formEl.append(buttonEL)
+  }
+
+  function cocktailFormGen() {
+    var divEl = $('<div>')
+    var formEl = $('<form>')
+    var h3El = $('<h3>')
+    var labelEl = $('<label>')
+    var inputEL = $('<input>')
+    var buttonEL = $('<button>')
+    var button2EL = $('<button>')
+  
+   divEl.attr( 'id',"form-2")
+   formEl.attr('id', "hungryForm")
+   h3El.text('Thirsty')
+   labelEl.attr('for', "drink-name")
+   labelEl.text('Enter Drink ')
+   inputEL.attr({'type':"text", 'id':"cocktail-input"})
+   
+   buttonEL.attr('id',"cocktails-submit")
+   buttonEL.text('Search')        
+   button2EL.attr({'id':"feeling-advent", 'class':'value=false'})
+   button2EL.text('Feeling Adventurous?')       
+  
+   rfContainer.append(divEl)
+    divEl.append(formEl)
+    formEl.append(h3El)
+    formEl.append(labelEl)
+    labelEl.append(inputEL)
+    formEl.append(buttonEL)
+    formEl.append(button2EL)
+  }
