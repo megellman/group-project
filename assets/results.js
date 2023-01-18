159dbd6a4ef692f963ee551c5df85bb7ec9f6e18
Michael Reyna <25.27.reyna@gmail.com>
2023-01-17 20:48:20 -0700

    featured style

--- assets/results.js
+++ assets/results.js
@@ -336,7 +336,7 @@ resultsContainer.on('click', '.saveBtn', function () {
     var input = $('<input>');
     var select = $('<datalist>');
     var submit = $('<input>');
-    backButton.text('Go Back')
+    backButton.text('X')
     backButton.attr({
       class: 'grid justify-items-start flex item-start box-border p-3',
     });
@@ -356,7 +356,7 @@ resultsContainer.on('click', '.saveBtn', function () {
     form.append(backButton, input, submit, select);
     currentContainer.append(form);
     backButton.on('click', function(){
-          currentContainer.remove('#form')
+      $('#form').remove()
           visBtn = false
         });
     // Get formObj from local storage, OR if that key doesn't exist, console waiting message
